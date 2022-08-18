cafc8d7218624d1a8d977b2c16654baea044a057
Stu Bolderson <stu.bolderson@aol.com>
2022-08-18 20:59:29 +0200

    base setup complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 // npm
 
 // files
-import { useModal } from "./context/ModalContext";
 import "./styles/main.scss";
 
 export default function App() {
-  const { setModal } = useModal();
-
-  return (
-    <div className="App">
-      <h2>Some title</h2>
-      <button onClick={() => setModal(<h1>Hewllo Modal</h1>)}>open form</button>
-    </div>
-  );
+  return <div className="App">hello</div>;
 }
