c1282674d410675ecd1ca14059458c2c7e05e760
Stu Bolderson <stu.bolderson@aol.com>
2022-08-20 09:10:45 +0200

    fixed up parralax scroll for story image

--- src/components/StoryContainer.jsx
+++ src/components/StoryContainer.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 export default function StoryContainer({ story }) {
-  const { title, text_1, text_2, text_3, btn, image } = story;
+  const { title, text_1, text_2, text_3, btn, imgPath } = story;
   return (
     <div className="story">
       <div className="content">
@@ -9,15 +7,14 @@ export default function StoryContainer({ story }) {
           <h2 className="heading-title">{title}</h2>
         </div>
         <div className="story__content--body">
-          {text_1 && <p>{text_1}</p>}
-          {text_2 && <p>{text_2}</p>}
-          {text_3 && <p>{text_3}</p>}
+          <p>{text_1}</p>
+          <p>{text_2}</p>
+          <p>{text_3}</p>
         </div>
         {btn && <button>{btn}</button>}
       </div>
-      <div className="image">
-        <img src={require(`../assets/images/${image}`)} alt={title} />
-      </div>
+      <div className={`image ${imgPath}`} />
+      {/* <img src={require(`../assets/images/${image}`)} alt={title} /> */}
     </div>
   );
 }
