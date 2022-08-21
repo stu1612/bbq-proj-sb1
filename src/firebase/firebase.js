e3af59869806181b72503f59214ba669b81e506d
Stu Bolderson <stu.bolderson@aol.com>
2022-08-21 11:08:29 +0200

    firbase config added

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHT-PFWLQoiItANGDBlXPgFkvfuy8uKhQ",
+  authDomain: "bbq-proj-sb1.firebaseapp.com",
+  projectId: "bbq-proj-sb1",
+  storageBucket: "bbq-proj-sb1.appspot.com",
+  messagingSenderId: "280852265636",
+  appId: "1:280852265636:web:ea7015e403693252444431",
+};
+
+const app = initializeApp(firebaseConfig);
+const fireStore = getFirestore(app);
+const auth = getAuth(app);
+
+export { fireStore, auth };
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+// npm
+import { useState } from "react";
+
+// files
+import InputField from "../components/InputField";
+import setup from "../data/login.json";
+
+// methods
+function onSubmit() {}
 
 export default function Admin() {
-  return <div>Admin</div>;
+  // local state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <section>
+      <div className="admin-form">
+        <form onSubmit={onSubmit}>
+          <InputField setup={setup.email} state={[email, setEmail]} />
+          <InputField setup={setup.password} state={[password, setPassword]} />
+          <button>Login</button>
+        </form>
+      </div>
+    </section>
+  );
 }
