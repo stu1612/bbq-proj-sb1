71b61a3decdc29ad77806d2804a3938a3a7b66a5
Stu Bolderson <stu.bolderson@aol.com>
2022-08-21 12:19:07 +0200

    base auth functionality added and working

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -4,21 +4,29 @@ import { useState } from "react";
 // files
 import InputField from "../components/InputField";
 import setup from "../data/login.json";
-
-// methods
-function onSubmit() {}
+import useLogin from "../hooks/useLogin";
 
 export default function Admin() {
+  const { login, error, loading } = useLogin();
   // local state
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  // methods
+  function onSubmit(event) {
+    event.preventDefault();
+    login(email, password);
+  }
+
   return (
     <section>
       <div className="admin-form">
+        {loading && <p>Loading ..</p>}
         <form onSubmit={onSubmit}>
           <InputField setup={setup.email} state={[email, setEmail]} />
           <InputField setup={setup.password} state={[password, setPassword]} />
           <button>Login</button>
+          {error && <p>{error}</p>}
         </form>
       </div>
     </section>
