d2cbcc35bc87ae8cb9a7b3fa246ac2a98683c91e
Stu Bolderson <stu.bolderson@aol.com>
2022-08-23 11:06:16 +0200

    add item forms working

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,10 +1,11 @@
 // npm
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 // files
 import InputField from "../components/InputField";
 import data from "../data/product.json";
+import useCreate from "../hooks/useCreateDocs";
 
 export default function ProductForm() {
   // local state
@@ -16,12 +17,27 @@ export default function ProductForm() {
   const [image, setImage] = useState("");
 
   // properties
+  const { addDocumentToCollection, response } = useCreate();
   const { title } = useParams();
+  const path = `menu/categories/content/${title}/content`;
 
   // methods
   function handleSubmit(event) {
     event.preventDefault();
+    const id = subTitle;
+    const doc = { subTitle, info, price, recipe, thumbnail, image };
+    addDocumentToCollection(path, id, doc);
   }
+
+  useEffect(() => {
+    if (response.success) {
+      setSubTitle("");
+      setInfo("");
+      setPrice("");
+      setRecipe("");
+    }
+  }, [response.success]);
+
   return (
     <form onSubmit={handleSubmit}>
       <InputField setup={data.title} state={[subTitle, setSubTitle]} />
