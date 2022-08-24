ca99c1394421b4b34d71630a24fcd323089db0a5
Stu Bolderson <stu.bolderson@aol.com>
2022-08-24 09:50:06 +0200

    categoryForm updated and complete with comments

--- src/components/CategoryForm.jsx
+++ src/components/CategoryForm.jsx
@@ -2,10 +2,10 @@
 import { useState, useEffect } from "react";
 
 // files
-import InputField from "../components/InputField";
-import data from "../data/category.json";
-import useFirebase from "../hooks/useFirebase";
 import { createFile } from "../firebase/cloudStorage";
+import data from "../data/category.json";
+import InputField from "../components/InputField";
+import useFirebase from "../hooks/useFirebase";
 
 export default function CategoryForm() {
   // local state
@@ -16,14 +16,12 @@ export default function CategoryForm() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [isValid, setIsValid] = useState(null);
-  // const [error, setError] = useState(null);
-
-  const path = "menu/categories/content";
-  const imgPath = `assets/image-${title}.png`;
 
   // properties
   const { addDocument, response } = useFirebase();
 
+  const path = "menu/categories/content";
+  const imgPath = `assets/image-${title}.png`;
   const types = ["image/png", "image/jpeg", "image/jpg"];
 
   function fileHandler(event) {
@@ -39,16 +37,17 @@ export default function CategoryForm() {
     }
   }
 
+  // upload file img to cloud storage when file dependancy changes and then set value to thumbnail state
   useEffect(() => {
-    async function loadImage(path) {
-      // const imgPath = `assets/image-${title}.png`;
-      if (isValid) {
-        await createFile(path, file).then((res) => setThumbnail(res));
+    async function loadImage(ref) {
+      if (file) {
+        await createFile(ref, file).then((res) => setThumbnail(res));
       } else setThumbnail(null);
     }
-    loadImage(`assets/image-${title}.png`);
-  }, [isValid]);
+    loadImage(imgPath);
+  }, [file]);
 
+  // if dispatch is success then reset values
   useEffect(() => {
     if (response.success) {
       setTitle("");
@@ -57,7 +56,7 @@ export default function CategoryForm() {
     }
   }, [response.success]);
 
-  // methods
+  // if file input is valid then dispatch payload to database
   async function handleSubmit(event) {
     event.preventDefault();
     setLoading(true);
@@ -78,6 +77,7 @@ export default function CategoryForm() {
 
   return (
     <form onSubmit={handleSubmit}>
+      {loading && <p>Loading ...</p>}
       <InputField setup={data.title} state={[title, setTitle]} />
       <InputField setup={data.info} state={[info, setInfo]} />
       <input type="file" accept="/image/*" onChange={fileHandler} required />
