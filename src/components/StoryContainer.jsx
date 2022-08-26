8fb50da9f016793436f2a5a4c325a4dee50cb911
Stu Bolderson <stu.bolderson@aol.com>
2022-08-26 14:47:56 +0200

    stories.json data file updated to clean up jsx import props

--- src/components/StoryContainer.jsx
+++ src/components/StoryContainer.jsx
@@ -1,15 +1,25 @@
+// npm
+import { useNavigate } from "react-router-dom";
+
 export default function StoryContainer({ story }) {
-  const { title, text_1, text_2, text_3, btn, imgPath } = story;
+  // properties
+  const { title, text, btn, imgPath } = story;
+  const navigate = useNavigate();
+
+  // components
+  const TextContent =
+    text && text.map((txt, index) => <p key={index}>{txt}</p>);
+
   return (
     <div className="story">
       <div className="story__body">
         <h2 className="heading-title">{title}</h2>
-        <div className="story__body--text">
-          <p>{text_1}</p>
-          <p>{text_2}</p>
-          <p>{text_3}</p>
-        </div>
-        {btn && <button className="btn">{btn}</button>}
+        <div className="story__body--text">{TextContent}</div>
+        {btn && (
+          <button className="btn" onClick={() => navigate(`/${imgPath}`)}>
+            {btn}
+          </button>
+        )}
       </div>
       <div className={`story__image ${imgPath}`} />
     </div>
